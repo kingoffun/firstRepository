7be519ab3f7870dae1c42a7a66c639f460208c76
kingoffun <kingoffun@yandex.ua>
2015-06-09 22:00:13 +0300

    add RLE.cs

---
+++ dataCoding/dataCoding/RLE.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataCoding
+{
+    class RLE
+    {
+        public string RleMessage { get; set; }
+
+        public string GetRLECode(string str )
+        {
+            int index = 1;
+            string rleCode = "";
+            
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i].Equals(str[i + 1]))
+                {
+                    if (i != str.Length - 2)
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        index++;
+                        rleCode = rleCode + index + str[i];
+                    }
+
+                }
+                else
+                {
+                    if (i != str.Length - 2)
+                    {
+                        rleCode = rleCode + index + str[i];
+                        index = 1;
+                    }
+                    else
+                    {
+                        rleCode = rleCode + index + str[i];
+                        rleCode = rleCode + 1 + str[i + 1];
+                    }
+                }
+            }
+
+            return rleCode;
+        }
+    }
+}
